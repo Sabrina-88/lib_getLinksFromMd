a697d6aad20c34c1ab3337022d61b67f2da3758a
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-09-04 20:19:04 -0300

    first commit

---
+++ test/getLinksFromMd.spec.js
@@ -0,0 +1,46 @@
+var mocha = require("mocha");
+var chai = require("chai");
+var url = require("./getLinksFromMd");
+var expect = chai.expect;
+
+describe("url", function() {
+  describe("#findURLsInText", function() {
+    describe("when there is no url in the text", function() {
+      it("should return an empty array", function() {
+
+      });
+    })
+
+    describe("when there is one url in the text", function() {
+      describe("and it has HTTPS", function() {
+        it("should return the url in an array", function() {
+
+        });
+      });
+
+      describe("and it has HTTP", function() {
+        it("should return the url in an array", function() {
+
+        });
+      });
+
+      describe("and it has www", function() {
+        it("should return the url in an array", function() {
+
+        });
+      });
+
+      describe("and it only the domain", function() {
+        it("should return the url in an array", function() {
+
+        });
+      });
+    })
+
+    describe("when there are three urls in the text", function() {
+      it("should return the urls in an array", function() {
+
+      });
+    })
+  });
+});
